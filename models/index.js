540d018af2ea9d5ef26b014d22914e96db98e443
menthus123 <MarvelJohnson@Hotmail.com>
2021-12-16 14:36:26 -0800

    Fixed models/index.js QuizQuestion assignment

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const QuizQuestionSchema = require('./quiz_question');
 const QuizQuestionChoiceSchema = require('./quiz_question_choice');
 
 const Quiz = mongoose.model('Quiz', QuizSchema);
-const QuizQuestion = mongoose.model('QuizQuestion', QuizQuestionChoiceSchema);
+const QuizQuestion = mongoose.model('QuizQuestion', QuizQuestionSchema);
 const QuizQuestionChoice = mongoose.model(
   'QuizQuestionChoice',
   QuizQuestionChoiceSchema
