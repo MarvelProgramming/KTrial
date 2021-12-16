51f61598cc7ba16db57b50055aae9d421e06a872
menthus123 <MarvelJohnson@Hotmail.com>
2021-12-16 13:37:56 -0800

    Updated models/quiz_question.js

--- models/quiz_question.js
+++ models/quiz_question.js
@@ -4,6 +4,8 @@ const QuizQuestionSchema = new Schema(
   {
     question: { type: String, required: true },
     description: { type: String, required: true },
+    timesAnsweredCorrectly: { type: Number, required: true },
+    timesAnsweredIncorrectly: { type: Number, required: true },
     choices: [{ type: Schema.Types.ObjectId, ref: 'QuizQuestionChoice' }]
   },
   { timestamps: true }
