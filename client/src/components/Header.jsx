08542798d33f4fff7d88baecca66cc0d193403a6
menthus123 <MarvelJohnson@Hotmail.com>
2021-12-17 12:37:53 -0800

    Added HamburgerMenu component to Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from './Nav';
+import HamburgerMenu from './HamburgerMenu';
 
 export default function Header() {
   return (
     <header className="primary-header">
       <Nav />
+      <HamburgerMenu />
       <i class="fas fa-user-circle"></i>
     </header>
   );
