0937c3156a07bf50ac4610ffc9609cfd3dcf61ff
menthus123 <MarvelJohnson@Hotmail.com>
2021-12-17 11:00:04 -0800

    Updated Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className="header">
-      
-    </div>
-  )
-}
+    <header className="primary-header">
+
+    </header>
+  );
+};
