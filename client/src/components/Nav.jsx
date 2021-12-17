c14413808bbba2ae1491b7bbc5c463886ee4cc49
menthus123 <MarvelJohnson@Hotmail.com>
2021-12-17 11:41:34 -0800

    Added links to Nav component

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
     <nav className="primary-nav">
-      
+      <Link to="/browse">Browse</Link>
+      <Link to="/quiz/creation">Create Quiz</Link>
+      <Link to="/about">About</Link>
     </nav>
   );
 };
