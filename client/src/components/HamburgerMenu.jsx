8c0a0383f3ea42d983ff2bbf764a5b5265d1299a
menthus123 <MarvelJohnson@Hotmail.com>
2021-12-17 12:30:30 -0800

    Added HamburgerMenu component

---
+++ client/src/components/HamburgerMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HamburgerMenu() {
+  return (
+    <div className="hamburger-menu-container">
+      <div className="hamburger-menu"></div>
+    </div>
+  )
+}
