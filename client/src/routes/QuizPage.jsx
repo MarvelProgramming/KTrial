d02707e26f08c9b344de58769beb687e4e5eb93c
menthus123 <MarvelJohnson@Hotmail.com>
2021-12-20 07:20:00 -0800

    Setup initial styling for Quiz_Finished

--- client/src/routes/QuizPage.jsx
+++ client/src/routes/QuizPage.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from '../components/Header'
 import Quiz_Start from '../components/Quiz_Start'
 import Quiz_Question from '../components/Quiz_Question';
+import Quiz_Finished from '../components/Quiz_Finished';
 
 export default function QuizPage() {
   return (
   <div className="quiz-page">
-    <Header />
-    <Quiz_Question />
+    {/* <Header /> */}
+    <Quiz_Finished />
   </div>
   );
 };
