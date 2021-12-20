a34444ea1857401757755737206e0dad2811f8be
menthus123 <MarvelJohnson@Hotmail.com>
2021-12-19 22:20:57 -0800

    Setup initial styling for QuizCreation_Review

---
+++ client/src/components/QuizCreation_Review.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function QuizCreation_Review() {
+  return (
+    <div className="quiz-creation-review">
+      <div>
+        <h1>Review</h1>
+        <p>Quiz Name: YourQuizName</p>
+        <p>Description: The description for your quiz.</p>
+        <p>Questions: 1</p>
+        <p>Is Public: true</p>
+        <button className="hollow-btn">Back</button>
+        <button className="filled-btn">Done</button>
+      </div>
+    </div>
+  )
+}
--- client/src/routes/QuizCreationPage.jsx
+++ client/src/routes/QuizCreationPage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import QuizCreation_Start from "../components/QuizCreation_Start";
 import QuizCreation_Questions from "../components/QuizCreation_Questions";
+import QuizCreation_Review from '../components/QuizCreation_Review';
 
 export default function QuizCreationPage () {
-  return <QuizCreation_Questions />
+  return <QuizCreation_Review />
 };
