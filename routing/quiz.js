e0ffa9345c826cf0b4fc12c6954044ae6af15e87
menthus123 <MarvelJohnson@Hotmail.com>
2021-12-20 17:03:47 -0800

    Added route to createQuiz

--- routing/quiz.js
+++ routing/quiz.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('../controllers/quiz');
 
 router.get('/range', controller.getRangeOfQuizzes);
+router.post('/', controller.createQuiz);
 
 module.exports = router;
